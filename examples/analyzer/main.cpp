d1a3ea29d39c0277bdc3f6b7acbd87d64759faa3
Vincent Hamp <vincent.hamp@higaski.at>
2025-11-01 11:55:51 +0100

    where is my close button?

--- examples/analyzer/main.cpp
+++ examples/analyzer/main.cpp
@@ -96,9 +96,11 @@ int main(int, char*[]) {
   ImGui_ImplOpenGL3_Init(glsl_version);
 
   // Our state
-  bool show_demo_window{true};
-  bool show_another_window{false};
   ImVec4 clear_color{0.45f, 0.55f, 0.60f, 1.00f};
+
+  bool show_config{true};
+  bool show_encode{true};
+  bool show_decode{true};
 
   // Disable .ini
   io.IniFilename = nullptr;
@@ -140,56 +142,26 @@ int main(int, char*[]) {
     ImGui::NewFrame();
     ImGui::DockSpaceOverViewport();
 
-    // 1. Show the big demo window (Most of the sample code is in
-    // ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear
-    // ImGui!).
-    if (show_demo_window) ImGui::ShowDemoWindow(&show_demo_window);
+    if (ImGui::BeginMainMenuBar()) {
+      if (ImGui::BeginMenu("View")) {
+        if (ImGui::MenuItem("Config", nullptr, &show_config)) {
+          // Handle New
+        }
+        if (ImGui::MenuItem("Encode", nullptr, &show_encode)) {
+          // Handle Open
+        }
+        if (ImGui::MenuItem("Decode", nullptr, &show_decode)) {
+          // Handle Save
+        }
+        ImGui::EndMenu();
+      }
 
-    // 2. Show a simple window that we create ourselves. We use a Begin/End pair
-    // to create a named window.
-    {
-      static float f = 0.0f;
-      static int counter = 0;
-
-      ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!"
-                                     // and append into it.
-
-      ImGui::Text("This is some useful text."); // Display some text (you can
-                                                // use a format strings too)
-      ImGui::Checkbox(
-        "Demo Window",
-        &show_demo_window); // Edit bools storing our window open/close state
-      ImGui::Checkbox("Another Window", &show_another_window);
-
-      ImGui::SliderFloat("float",
-                         &f,
-                         0.0f,
-                         1.0f); // Edit 1 float using a slider from 0.0f to 1.0f
-      ImGui::ColorEdit3(
-        "clear color",
-        (float*)&clear_color); // Edit 3 floats representing a color
-
-      if (ImGui::Button("Button")) // Buttons return true when clicked (most
-                                   // widgets return true when edited/activated)
-        counter++;
-      ImGui::SameLine();
-      ImGui::Text("counter = %d", counter);
-
-      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
-                  1000.0f / io.Framerate,
-                  io.Framerate);
-      ImGui::End();
+      ImGui::EndMainMenuBar();
     }
 
-    // 3. Show another simple window.
-    if (show_another_window) {
-      ImGui::Begin(
-        "Another Window",
-        &show_another_window); // Pass a pointer to our bool variable (the
-                               // window will have a closing button that will
-                               // clear the bool when clicked)
-      ImGui::Text("Hello from another window!");
-      if (ImGui::Button("Close Me")) show_another_window = false;
+    if (show_config) {
+      ImGui::Begin("Config");
+      ImGui::Text("Hello from another window!", &show_config);
       ImGui::End();
     }
 
